3080cc7cb0ab46026c5b458acfbf8151f20e8900
Yura192c <fpm.mironchiYU@bsu.by>
2023-10-01 14:49:28 +0300

    split the settings

---
+++ config/settings/development.py
@@ -0,0 +1,10 @@
+from .base import *  # подгружаем настройки по умолчанию
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-1f3^6s1!u54cxdi!+5!-_wjq*!#$vfmc%af)#xy+f$53@v7c(^'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
---
+++ config/settings/production.py
@@ -0,0 +1,21 @@
+from .base import *  # подгружаем настройки по умолчанию
+import os
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", False))
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = []
